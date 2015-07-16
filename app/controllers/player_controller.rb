6cd592afbb0deefad2643a4b64c966fa422f6ac4
Ryan Whiley <whileyryan@gmail.com>
2015-07-15 22:31:28 -0700

    order by year

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,6 +1,6 @@
 class PlayerController < ApplicationController
 	def show
-		@seasons = Point.where(player_id: params[:id])
+		@seasons = Point.where(player_id: params[:id], points: 0).order(:year)
 		@player = Player.find(params[:id])
 		@team = Team.find(@player.team_id)
 	end

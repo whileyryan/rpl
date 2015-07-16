2cf735af63758c6aa44150f68c25fde01f9d5514
Ryan Whiley <whileyryan@gmail.com>
2015-07-15 22:34:20 -0700

    fixed bug

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,6 +1,6 @@
 class PlayerController < ApplicationController
 	def show
-		@seasons = Point.where(player_id: params[:id], points: 0).order(:year)
+		@seasons = Point.where(player_id: params[:id], points: 0..Float::INFINITY).order(:year)
 		@player = Player.find(params[:id])
 		@team = Team.find(@player.team_id)
 	end
